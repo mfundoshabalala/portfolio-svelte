185170dfd52cd974dbbc9c4fd91a497b5fe18960
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-16 17:42:29 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script></script>
+
+<header class="flex justify-around top-0 left-0 py-6 fixed w-full bg-white shadow">
+	<!-- brand logo -->
+	<div class="text-3xl font-black">
+		<span class="">Mfundo</span>
+		<span class="">Shabalala</span>
+	</div>
+	<!-- hamburger menu -->
+	<div class="">
+		<span class="" />
+		<span class="" />
+		<span class="" />
+	</div>
+	<!-- navigation -->
+	<nav class="">
+		<ul class="flex gap-4 font-light">
+			<li class="">
+				<a href="#section1" class="border rounded shadow-sm px-4 py-2">Home</a>
+			</li>
+			<li class="">
+				<a href="#section2" class="border rounded shadow-sm px-4 py-2">Projects</a>
+			</li>
+			<li class="">
+				<a href="#section3" class="border rounded shadow-sm px-4 py-2">Experience</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<main class="">
+	<!-- about us section -->
+	<section id="section1" class="">
+		<h2 class="text-xl font-bold">Section 1</h2>
+	</section>
+	<!-- projects section -->
+	<section id="section2" class="">
+		<h2 class="text-xl font-bold">Section 2</h2>
+	</section>
+	<!-- jobs and education section -->
+	<section id="section3" class="">
+		<h2 class="text-xl font-bold">Section 3</h2>
+	</section>
+</main>
+
+<footer class="py-6 text-center">&copy;2020-</footer>
+
+<style>
+</style>
