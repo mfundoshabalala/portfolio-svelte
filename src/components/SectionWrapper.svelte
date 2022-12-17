7004f4c06a8685330dac93fc5c443474131cf091
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-17 19:40:10 +0200

    Create SectionWrapper.svelte

---
+++ src/components/SectionWrapper.svelte
@@ -0,0 +1,18 @@
+<script>
+	let id = '';
+</script>
+
+<section {id}>
+	<slot name="link">No nav link</slot>
+	<slot />
+</section>
+
+<style>
+	section {
+		@apply relative min-h-screen flex flex-col justify-center items-center;
+	}
+
+	section > :global(div) {
+		@apply absolute -top-10;
+	}
+</style>
