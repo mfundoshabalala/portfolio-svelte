9f65d16830c4f9d3a131267e4b7a87b7b06b9115
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-17 19:40:20 +0200

    Create AboutMe.svelte

---
+++ src/layout/AboutMe.svelte
@@ -0,0 +1,12 @@
+<script>
+	import SectionWrapper from '../components/SectionWrapper.svelte';
+</script>
+
+<SectionWrapper>
+	<div slot="link" id="section3" />
+	<h2>About Me</h2>
+</SectionWrapper>
+
+<style>
+	/* your styles go here */
+</style>
