2d403dabf06945814764c80b8777562e833e57ab
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-17 19:40:44 +0200

    Create Navbar.svelte

---
+++ src/layout/Navbar.svelte
@@ -0,0 +1,41 @@
+<script>
+	// your script goes here
+</script>
+
+<header>
+	<!-- brand logo -->
+	<div class="text-3xl font-black">
+		<span class="">Mfundo</span>
+		<span class="">Shabalala</span>
+	</div>
+	<!-- hamburger menu -->
+	<div class="">
+		<span class="" />
+		<span class="" />
+		<span class="" />
+	</div>
+	<!-- navigation -->
+	<nav class="">
+		<ul class="flex gap-4 font-light">
+			<li class="">
+				<a href="#section1">Home</a>
+			</li>
+			<li class="">
+				<a href="#section2">My Work</a>
+			</li>
+			<li class="">
+				<a href="#section3">About Me</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	header {
+		@apply flex justify-around top-0 left-0 py-6 fixed w-full shadow bg-slate-900 z-50;
+	}
+
+	a {
+		@apply border rounded shadow-sm px-4 py-2 text-xl;
+	}
+</style>
