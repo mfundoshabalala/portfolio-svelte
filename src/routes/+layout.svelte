09032f5e13445f7c916a4f81109c249d18b5ae4a
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-17 19:41:04 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
-  import "../app.css";
+	import Navbar from '../layout/Navbar.svelte';
+	import '../app.css';
+	import Footer from '../layout/Footer.svelte';
+	import Aside from '../layout/Aside.svelte';
 </script>
 
+<svelte:head>
+	<!-- fontawesome cdn -->
+	<link
+		rel="stylesheet"
+		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+	/>
+	<title>Mfundo's Web Portfolio</title>
+</svelte:head>
+
+<!--  -->
+<Navbar />
+<!--  -->
+<Aside />
+<!--  -->
 <slot />
+<!--  -->
+<Footer />
