519311abbaf7e2e3131623cd953fb4a5714aaf7f
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-17 19:40:35 +0200

    Create LandingSection.svelte

---
+++ src/layout/LandingSection.svelte
@@ -0,0 +1,33 @@
+<script>
+	import moment from 'moment';
+	import SectionWrapper from '../components/SectionWrapper.svelte';
+
+	const duration = moment([2020, 6, 20]).toNow(true);
+	const fullName = 'Mfundo Shabalala';
+</script>
+
+<SectionWrapper>
+	<div slot="link" id="section1" />
+	<h1>
+		<span>Hello! My name is</span>
+		<span>{fullName}</span>
+	</h1>
+	<p>
+		I am a web developer with {duration} of experience building websites and web applications. I am skilled
+		in frontend and backend development, and have a passion for creating intuitive, user-friendly interfaces.
+	</p>
+</SectionWrapper>
+
+<style>
+	h1 {
+		@apply text-xl font-black flex flex-col justify-center items-center;
+	}
+
+	span:last-child {
+		@apply text-7xl;
+	}
+
+	p {
+		@apply max-w-2xl font-light px-6;
+	}
+</style>
