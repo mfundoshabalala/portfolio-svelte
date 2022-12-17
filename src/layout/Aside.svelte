cabf039cee46b2875557552062bf3ba6a3ca6978
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-17 19:40:26 +0200

    Create Aside.svelte

---
+++ src/layout/Aside.svelte
@@ -0,0 +1,35 @@
+<script>
+	// your script goes here
+</script>
+
+<aside class="fixed top-2/4 left-6">
+	<ul class="flex flex-col gap-2">
+		<li class="">
+			<a href="https://github.com/mfundoshabalala">
+				<i class="fab fa-github" />
+			</a>
+		</li>
+		<li class="">
+			<a href="https://linkedin.com/in/mfundoshabalala">
+				<i class="fab fa-linkedin-in" />
+			</a>
+		</li>
+		<li class="">
+			<a href="https://twitter.com/mfundoshabalal">
+				<i class="fab fa-twitter" />
+			</a>
+		</li>
+		<li class="">
+			<a href="mailto:mfundoshabalala@gmail.com">
+				<i class="fa fa-envelope" />
+			</a>
+		</li>
+		<li />
+	</ul>
+</aside>
+
+<style>
+	aside {
+		@apply text-xl;
+	}
+</style>
