cda2891f542d4afb2884ae3f026d922d75bef37e
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-19 06:36:26 +0200

    Update SectionWrapper.svelte

--- src/components/SectionWrapper.svelte
+++ src/components/SectionWrapper.svelte
@@ -1,10 +1,13 @@
 <script>
-	let id = '';
+	export let id = '';
 </script>
 
 <section {id}>
 	<slot name="link">No nav link</slot>
-	<slot />
+	<slot name="title">No title</slot>
+	<slot name="content">
+		<h2>Failed to load this section</h2>
+	</slot>
 </section>
 
 <style>
