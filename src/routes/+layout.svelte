a8ee8d77145a1d86f69e5a0ec00e6d184b5aadb2
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-19 06:36:30 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,13 @@
 		rel="stylesheet"
 		href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
 	/>
+
+	<!-- devicon cdn -->
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+	/>
+
 	<title>Mfundo's Web Portfolio</title>
 </svelte:head>
 
