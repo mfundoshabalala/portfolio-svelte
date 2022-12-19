8c71c84c1e2f63b024e3a22e862a8c9d408a67f8
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-19 06:36:41 +0200

    Update Aside.svelte

--- src/layout/Aside.svelte
+++ src/layout/Aside.svelte
@@ -5,22 +5,22 @@
 <aside class="fixed top-2/4 left-6">
 	<ul class="flex flex-col gap-2">
 		<li class="">
-			<a href="https://github.com/mfundoshabalala">
+			<a target="_blank" rel="noreferrer" href="https://github.com/mfundoshabalala">
 				<i class="fab fa-github" />
 			</a>
 		</li>
 		<li class="">
-			<a href="https://linkedin.com/in/mfundoshabalala">
+			<a target="_blank" rel="noreferrer" href="https://linkedin.com/in/mfundoshabalala">
 				<i class="fab fa-linkedin-in" />
 			</a>
 		</li>
 		<li class="">
-			<a href="https://twitter.com/mfundoshabalal">
+			<a target="_blank" rel="noreferrer" href="https://twitter.com/mfundoshabalal">
 				<i class="fab fa-twitter" />
 			</a>
 		</li>
 		<li class="">
-			<a href="mailto:mfundoshabalala@gmail.com">
+			<a target="_blank" href="mailto:mfundoshabalala@gmail.com">
 				<i class="fa fa-envelope" />
 			</a>
 		</li>
@@ -30,6 +30,14 @@
 
 <style>
 	aside {
-		@apply text-xl;
+		@apply text-xl z-50;
+	}
+
+	a {
+		@apply cursor-pointer;
+	}
+
+	i {
+		@apply w-5 h-5;
 	}
 </style>
