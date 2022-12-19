d2493e1514cfecde67ac8e45ec03bbf9695f1164
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-19 06:36:45 +0200

    Update Footer.svelte

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -1,8 +1,11 @@
 <script>
-	// your script goes here
+	const date = new Date();
+	const year = date.getFullYear();
 </script>
 
-<footer class="py-6 text-center">&copy;2020-</footer>
+<footer class="py-6 text-center font-light">
+	&copy;2020-{year} Made using Svelte, TypeScript and Tailwindcss
+</footer>
 
 <style>
 	/* your styles go here */
