ca028989cbd33111cbc7cd70877a3b21567913e2
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-23 06:15:55 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,13 @@
 		rel="stylesheet"
 		href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
 	/>
+	<!-- google fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Aguafina+Script&family=Arizonia&family=Comforter&family=Comforter+Brush&family=Cookie&family=Corinthia:wght@700&family=Ma+Shan+Zheng&family=Molle&family=Mrs+Sheppards&family=Norican&family=Praise&family=Ruthie&family=Smooch&family=Tangerine:wght@700&family=Water+Brush&family=Yesteryear&display=swap"
+		rel="stylesheet"
+	/>
 
 	<title>Mfundo's Web Portfolio</title>
 </svelte:head>
