05f7f65bfde1324a7e2fe27213b17c1f222092b9
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-23 06:14:43 +0200

    Update SectionWrapper.svelte

--- src/components/SectionWrapper.svelte
+++ src/components/SectionWrapper.svelte
@@ -5,17 +5,29 @@
 <section {id}>
 	<slot name="link">No nav link</slot>
 	<slot name="title">No title</slot>
+	<slot name="description"
+		><p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur rerum iure nostrum
+			officiis deleniti placeat maxime maiores harum iusto, nulla veniam necessitatibus corporis
+			magni nobis aspernatur, explicabo, quas culpa dignissimos.
+		</p></slot
+	>
 	<slot name="content">
 		<h2>Failed to load this section</h2>
 	</slot>
+	<slot name="socials" />
 </section>
 
 <style>
 	section {
-		@apply relative min-h-screen flex flex-col justify-center items-center;
+		@apply relative min-h-screen flex flex-col justify-center items-center gap-2;
 	}
 
 	section > :global(div) {
 		@apply absolute -top-10;
+	}
+
+	p {
+		@apply max-w-2xl leading-5 font-thin;
 	}
 </style>
