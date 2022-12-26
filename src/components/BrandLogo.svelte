50bb995316642dc65db556db5571562f3d219932
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-26 23:40:25 +0200

    Create BrandLogo.svelte

---
+++ src/components/BrandLogo.svelte
@@ -0,0 +1,19 @@
+<script>
+	// your script goes here
+</script>
+
+<!-- brand logo -->
+<div>
+	<span class="">Mfundo</span>
+	<span class="">Shabalala</span>
+</div>
+
+<style>
+	div {
+		@apply text-4xl font-comforter flex gap-1 rotate-1 select-none;
+	}
+
+	div span:last-child {
+		@apply bg-gradient-to-r from-yellow-600 via-yellow-500 to-yellow-600 bg-clip-text text-transparent;
+	}
+</style>
