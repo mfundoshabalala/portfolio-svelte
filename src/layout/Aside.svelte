3677880f5e7f9556544a94e144d298a4a297477e
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-26 23:40:08 +0200

    Update Aside.svelte

--- src/layout/Aside.svelte
+++ src/layout/Aside.svelte
@@ -2,42 +2,59 @@
 	// your script goes here
 </script>
 
-<aside class="fixed top-2/4 left-6">
-	<ul class="flex flex-col gap-2">
-		<li class="">
+<aside>
+	<ul>
+		<li>
 			<a target="_blank" rel="noreferrer" href="https://github.com/mfundoshabalala">
-				<i class="fab fa-github" />
+				<i class="fab fa-github" /><span>Github</span>
 			</a>
 		</li>
-		<li class="">
+		<li>
 			<a target="_blank" rel="noreferrer" href="https://linkedin.com/in/mfundoshabalala">
-				<i class="fab fa-linkedin-in" />
+				<i class="fab fa-linkedin-in" /><span>LinkedIn</span>
 			</a>
 		</li>
-		<li class="">
+		<li>
 			<a target="_blank" rel="noreferrer" href="https://twitter.com/mfundoshabalal">
-				<i class="fab fa-twitter" />
+				<i class="fab fa-twitter" /><span>Twitter</span>
 			</a>
 		</li>
-		<li class="">
+		<li>
 			<a target="_blank" href="mailto:mfundoshabalala@gmail.com">
-				<i class="fa fa-envelope" />
+				<i class="fa fa-envelope" /><span>Email</span>
 			</a>
 		</li>
-		<li />
 	</ul>
 </aside>
 
 <style>
 	aside {
-		@apply text-xl z-50;
+		@apply fixed top-1/3 left-5 z-50 font-sans shadow-sm rounded-full shadow-yellow-500 w-12 h-48 py-4 bg-slate-900;
+	}
+
+	ul {
+		@apply flex flex-col justify-center h-full items-center text-xl gap-2;
+	}
+
+	/* li {
+		@apply relative block h-10;
 	}
 
 	a {
-		@apply cursor-pointer;
+		@apply relative cursor-pointer font-thin block w-full h-full leading-relaxed px-4 transition-all duration-500;
 	}
 
+	a:hover span {
+		@apply translate-x-9 inline-block;
+	}
+
+
+*/
 	i {
-		@apply w-5 h-5;
+		@apply aspect-square;
+	}
+
+	span {
+		@apply hidden;
 	}
 </style>
