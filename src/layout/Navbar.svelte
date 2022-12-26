d177af6942888b57cad4bfd7370956540c793f9d
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-26 23:39:49 +0200

    Update Navbar.svelte

--- src/layout/Navbar.svelte
+++ src/layout/Navbar.svelte
@@ -1,41 +1,20 @@
 <script>
-	// your script goes here
+	import Menu from '../components/Menu.svelte';
+	import BrandLogo from '../components/BrandLogo.svelte';
+	import Navigation from '../components/Navigation.svelte';
 </script>
 
 <header>
-	<!-- brand logo -->
-	<div class="text-3xl font-black">
-		<span class="">Mfundo</span>
-		<span class="">Shabalala</span>
-	</div>
+	<!-- Brand Logo -->
+	<BrandLogo />
 	<!-- hamburger menu -->
-	<div class="">
-		<span class="" />
-		<span class="" />
-		<span class="" />
-	</div>
+	<Menu />
 	<!-- navigation -->
-	<nav class="">
-		<ul class="flex gap-4 font-light">
-			<li class="">
-				<a href="#section1">Home</a>
-			</li>
-			<li class="">
-				<a href="#section2">My Work</a>
-			</li>
-			<li class="">
-				<a href="#section3">About Me</a>
-			</li>
-		</ul>
-	</nav>
+	<Navigation />
 </header>
 
 <style>
 	header {
-		@apply flex justify-around top-0 left-0 py-6 fixed w-full shadow bg-slate-900 z-50;
-	}
-
-	a {
-		@apply border rounded shadow-sm px-4 py-2;
+		@apply flex justify-around top-0 left-0 py-6 fixed w-full bg-slate-900 z-50;
 	}
 </style>
