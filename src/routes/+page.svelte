56336a3b86f62a1322fe59f76f0520267d0f53f3
Mfundo Bright Shabalala <mfundoshabalala@gmail.com>
2022-12-27 12:25:40 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import AboutMe from '../layout/AboutMe.svelte';
-	import Landing from '../layout/LandingSection.svelte';
+	import Landing from '../layout/Landing.svelte';
 	import Portfolio from '../layout/Portofolio.svelte';
 	import Skills from '../layout/Skills.svelte';
 </script>
